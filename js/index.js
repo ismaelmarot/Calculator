41c73eff429370831f8a5dbf5fc21097ef700447
ismaelmarot <ismaelmarot@hotmail.com>
2021-11-18 18:29:57 -0300

    Create methods add/substract/multiply/divide in Calculator.js file

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const displayPreviousValue = document.getElementById('id-prev-value');
+const displayCurrentValue = document.getElementById('id-curr-value');
+const buttonsNumbers = document.querySelectorAll('.number');
+const buttonsOperators = document.querySelectorAll('.operator');
+
+const calculator = new Calculator();
