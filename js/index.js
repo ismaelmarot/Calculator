50ba39ec04fe0d3c7006f3f4066ed9873b2fd876
ismaelmarot <ismaelmarot@hotmail.com>
2021-11-18 20:39:47 -0300

    Add operations and Display configuration

--- js/index.js
+++ js/index.js
@@ -3,4 +3,16 @@ const displayCurrentValue = document.getElementById('id-curr-value');
 const buttonsNumbers = document.querySelectorAll('.number');
 const buttonsOperators = document.querySelectorAll('.operator');
 
-const calculator = new Calculator();
+const display = new Display(displayPreviousValue,displayCurrentValue);
+
+buttonsNumbers.forEach(button =>{
+    button.addEventListener('click',()=>{
+        display.addNumber(button.innerHTML);
+    });
+});
+
+buttonsOperators.forEach(button =>{
+    button.addEventListener('click',()=>{
+        display.compute(button.value);
+    });
+});
